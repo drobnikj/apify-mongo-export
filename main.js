944d671f594959c05fb0dbbbd666a05418fb0c68
drobnikj <drobnik.j@gmail.com>
2019-12-04 09:23:06 +0100

    Added parsing a dates in input json

--- main.js
+++ main.js
@@ -3,31 +3,36 @@ const MongoClient = require('mongodb').MongoClient;
 const Promise = require('bluebird');
 const { createTunnel, closeTunnel } = require('proxy-chain');
 
+const ISODateRegExp = /^(\d{4})-(\d{2})-(\d{2})T(\d{2}):(\d{2}):(\d{2}(?:\.\d*))(?:Z|(\+|-)([\d|:]*))?$/;
 
 const tearDown = async (mongoClient, tunnel) => {
     await mongoClient.close();
     if (tunnel) await closeTunnel(tunnel);
 };
 
+const parseDates = (key, value) => {
+    if (typeof value === 'string' && ISODateRegExp.test(value)) {
+        return new Date(value);
+    }
+    return value;
+};
+
+/**
+ * Parses input of actor with all nested dates in it.
+ * @return {object}
+ */
+const getInputWithParsedDates = async () => {
+    const input = await Apify.getInput();
+    const inputJSON = JSON.stringify(input);
+    return JSON.parse(inputJSON, parseDates);
+};
+
 Apify.main(async () => {
-    let { dbName, query, fields = {}, collectionName, mongoUrl, concurrency = 10, useTunnel, estimatedItemsCount } = await Apify.getValue('INPUT');
+    let { dbName, query, fields = {}, collectionName, mongoUrl, concurrency = 10, useTunnel, estimatedItemsCount } = await getInputWithParsedDates();
 
     mongoUrl = process.env.MONGO_URL || mongoUrl;
 
     if (!query || !collectionName || !mongoUrl) throw new Error('Fields query, collectionName and mongoUrl are required!');
-
-    // TODO: Better datetime parsing
-    // Object.keys(query).forEach((key) => {
-    //     if (key.match(/At$/)) {
-    //         if (_.isObject(query[key])) {
-    //             Object.keys(query[key]).forEach((childKey) => {
-    //                 query[key][childKey] = new Date(query[key][childKey]);
-    //             });
-    //         } else {
-    //             query[key] = new Date(query[key]);
-    //         }
-    //     }
-    // });
 
     // Connect to MongoDB
     let tunnel;
@@ -36,8 +41,8 @@ Apify.main(async () => {
 
         const [wholeString, protocol, credentials, host, additionalDetails] = mongoUrl.match(/(mongodb):\/\/(.*)@([^/]*)\/?(.*)/);
 
-        // Create Tunel for mongoDb connection
-        console.log('Creating tunnel fo connection to mongo DB.')
+        // Create Tunnel for mongoDb connection
+        console.log('Creating tunnel fo connection to mongo DB.');
         tunnel = await createTunnel(proxyUrl, host);
         mongoUrl = `${protocol}://${credentials}@${tunnel}/${additionalDetails}`;
     }
